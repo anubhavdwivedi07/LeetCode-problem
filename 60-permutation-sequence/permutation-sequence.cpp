9e56e0253944c3f8307be119b6fc33fb4f015259
Anubhav Dwivedi <anubhavdwivedi1402@gmail.com>
2026-08-20 10:10:01 +0530

    Time: 71 ms (11.51%) | Memory: 8.9 MB (37.60%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+         vector<int> arr;
+        for (int i = 1; i <= n; i++) {
+            arr.push_back(i);
+        }
+
+       
+        for (int count = 1; count < k; count++) {
+            
+          
+            int pivot = -1;
+            for (int i = n - 2; i >= 0; i--) {
+                if (arr[i] < arr[i + 1]) {
+                    pivot = i;
+                    break;
+                }
+            }
+
+            if (pivot == -1) {
+                break;
+            }
+
+            int swapIdx = -1;
+            for (int i = n - 1; i > pivot; i--) {
+                if (arr[i] > arr[pivot]) {
+                    swapIdx = i;
+                    break;
+                }
+            }
+
+            swap(arr[pivot], arr[swapIdx]);
+
+            reverse(arr.begin() + pivot + 1, arr.end());
+        }
+
+       
+        string result;
+        for (int num : arr) {
+            result += to_string(num);
+        }
+        return result;
+    }
+};
